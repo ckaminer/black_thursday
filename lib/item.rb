9d29f87d8e10172dd06a3b4fc81995848c65f904
Nick Pisciotta <npisciotta@gmail.com>
2016-04-19 17:35:19 -0600

    Finished item repo and tests

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,6 @@
+require 'bigdecimal'
+require 'bigdecimal/util'
+
 class Item
 
   attr_reader :id, :name, :description, :unit_price,
@@ -7,33 +10,40 @@ class Item
     @id = row[:id]
     @name = row[:name]
     @description = row[:description]
-    @unit_price = row[:unit_price]
+    @unit_price = BigDecimal.new(row[:unit_price], 4)
     @merchant_id = row[:merchant_id]
     @created_at = row[:created_at]
     @updated_at = row[:updated_at]
   end
 
-  def id
-  end
-
-  def name
-  end
-
-  def description
-  end
-
-  def unit_price
-  end
-
-  def created_at
-  end
-
-  def updated_at
-  end
-
-  def merchant_id
-  end
-
-
+  # def id
+  # end
+  #
+  # def name
+  # end
+  #
+  # def description
+  # end
+  #
+  # def unit_price
+  # end
+  #
+  # def created_at
+  # end
+  #
+  # def updated_at
+  # end
+  #
+  # def merchant_id
+  # end
 
 end
+
+# ir = Item.new({
+#   :name        => "Pencil",
+#   :description => "You can use it to write things",
+#   :created_at  => Time.now,
+#   :updated_at  => Time.now,
+# })
+#
+# puts ir.description
