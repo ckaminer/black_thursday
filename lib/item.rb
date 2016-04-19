155d4b331db77c30a8175f380624051ecc06cbb3
Nick Pisciotta <npisciotta@gmail.com>
2016-04-19 17:47:36 -0600

    resolved conflicts

--- lib/item.rb
+++ lib/item.rb
@@ -16,34 +16,4 @@ class Item
     @updated_at = row[:updated_at]
   end
 
-  # def id
-  # end
-  #
-  # def name
-  # end
-  #
-  # def description
-  # end
-  #
-  # def unit_price
-  # end
-  #
-  # def created_at
-  # end
-  #
-  # def updated_at
-  # end
-  #
-  # def merchant_id
-  # end
-
 end
-
-# ir = Item.new({
-#   :name        => "Pencil",
-#   :description => "You can use it to write things",
-#   :created_at  => Time.now,
-#   :updated_at  => Time.now,
-# })
-#
-# puts ir.description
