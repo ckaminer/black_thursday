f43eac1f2df8c321bdf7daffa9c752e45ad1248b
Charles Kaminer <ckamine@g.clemson.edu>
2016-04-19 18:05:53 -0600

    tested merchant and sales engine

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -4,9 +4,15 @@ require './lib/merchant'
 class MerchantTest < Minitest::Test
 
   def test_merchant_has_id
+    m = Merchant.new({:id => 5, :name => "Turing School"})
+
+    assert_equal 5, m.id
   end
 
   def test_merchant_has_name
+    m = Merchant.new({:id => 5, :name => "Turing School"})
+
+    assert_equal "Turing School", m.name
   end
 
 end
